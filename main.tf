3136c2a308cf1f78745ed0d2f2146a72bac5cd78
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-07-10 09:45:16 +0100

    Update for tagging

--- main.tf
+++ main.tf
@@ -21,41 +21,8 @@ resource "aws_ssm_maintenance_window_target" "default" {
   resource_type = "INSTANCE"
   
   targets {
-    key    = "InstanceIds"
-    values = "${var.mi_list["week${count.index+1}"]}"
-  }
-}
-
-resource "aws_ssm_maintenance_window_task" "default_task_ssmagent" {
-  count            = "${var.weeks}"
-  window_id        = "${element(aws_ssm_maintenance_window.default.*.id, count.index)}"
-  name             = "update_ssm_agent"
-  description      = "Update SSM Agent"
-  task_type        = "RUN_COMMAND"
-  task_arn         = "AWS-UpdateSSMAgent"
-  priority         = 30
-  service_role_arn = "${var.role}"
-  max_concurrency  = "${var.mw_concurrency}"
-  max_errors       = "${var.mw_error_rate}"
-
-  logging_info {
-      s3_bucket_name = "${var.s3_bucket}"
-      s3_region = "${var.region}"
-      s3_bucket_prefix = "${var.weeks > 1 ? "${var.type}_linux_week-${count.index+1}_${var.day}_${var.hour}00/${var.account}-${var.environment}" : "${var.type}_linux_week-${var.week}_${var.day}_${var.hour}00/${var.account}-${var.environment}" }"
-  }
-
-  targets {
-    key    = "WindowTargetIds"
-    values = ["${element(aws_ssm_maintenance_window_target.default.*.id, count.index)}"]
-  }
-
-  task_parameters {
-    name   = "allowDowngrade"
-    values = ["false"]
-  }
-
-  lifecycle {
-    ignore_changes = ["task_parameters"]
+    key    = "tag:ssmMaintenanceWindow"
+    values = ["${var.weeks > 1 ? "${var.type}_week-${count.index+1}_${var.day}_${var.hour}00" : "${var.type}_week-${var.week}_${var.day}_${var.hour}00"}"]
   }
 }
 
@@ -66,7 +33,7 @@ resource "aws_ssm_maintenance_window_task" "default_task_updates" {
   description      = "Install YUM Updates"
   task_type        = "RUN_COMMAND"
   task_arn         = "AWS-RunShellScript"
-  priority         = 60
+  priority         = 10
   service_role_arn = "${var.role}"
   max_concurrency  = "${var.mw_concurrency}"
   max_errors       = "${var.mw_error_rate}"
@@ -99,7 +66,7 @@ resource "aws_ssm_maintenance_window_task" "default_task_email_notification" {
   description      = "Send email notification"
   task_type        = "RUN_COMMAND"
   task_arn         = "AWL-SSMEmailNotification"
-  priority         = 60
+  priority         = 20
   service_role_arn = "${var.role}"
   max_concurrency  = "${var.mw_concurrency}"
   max_errors       = "${var.mw_error_rate}"
@@ -113,6 +80,40 @@ resource "aws_ssm_maintenance_window_task" "default_task_email_notification" {
   targets {
     key    = "WindowTargetIds"
     values = ["${element(aws_ssm_maintenance_window_target.default.*.id, count.index)}"]
+  }
+
+  lifecycle {
+    ignore_changes = ["task_parameters"]
+  }
+}
+
+
+resource "aws_ssm_maintenance_window_task" "default_task_ssmagent" {
+  count            = "${var.weeks}"
+  window_id        = "${element(aws_ssm_maintenance_window.default.*.id, count.index)}"
+  name             = "update_ssm_agent"
+  description      = "Update SSM Agent"
+  task_type        = "RUN_COMMAND"
+  task_arn         = "AWS-UpdateSSMAgent"
+  priority         = 30
+  service_role_arn = "${var.role}"
+  max_concurrency  = "${var.mw_concurrency}"
+  max_errors       = "${var.mw_error_rate}"
+
+  logging_info {
+      s3_bucket_name = "${var.s3_bucket}"
+      s3_region = "${var.region}"
+      s3_bucket_prefix = "${var.weeks > 1 ? "${var.type}_linux_week-${count.index+1}_${var.day}_${var.hour}00/${var.account}-${var.environment}" : "${var.type}_linux_week-${var.week}_${var.day}_${var.hour}00/${var.account}-${var.environment}" }"
+  }
+
+  targets {
+    key    = "WindowTargetIds"
+    values = ["${element(aws_ssm_maintenance_window_target.default.*.id, count.index)}"]
+  }
+
+  task_parameters {
+    name   = "allowDowngrade"
+    values = ["false"]
   }
 
   lifecycle {
